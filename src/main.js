5fb65b2b90e3ce1a75a1601091d907d8cedd7f6f
TheStarUnderSky <771574316@qq.com>
2018-08-23 11:21:03 +0800

    todolist complete

--- src/main.js
+++ src/main.js
@@ -1,13 +1,37 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  showStatus: false,
+  mytodo: '',
+  data: {
+    todoList:
+    [
+      {text: '吃饭', done: false},
+      {text: '睡觉', done: false},
+      {text: '打豆豆', done: false}
+    ]
+  },
+  computed: {
+    remain () {
+      return this.todoList.filter(v => !v.done).length
+    }
+  },
+  methods: {
+    handleClick () {
+      this.todoList.push({text: this.mytodo, done: false})
+      this.mytodo = ''
+    },
+    toggle (i) {
+      this.todoList[i].done = !this.todoList[i].done
+    },
+    clean () {
+      this.todoList = this.todoList.filter(v => !v.done)
+    }
+  }
 })
